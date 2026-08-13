0ec9e0a8dd848e1003dde0dc7bdd24fe0c568ca1
maulvi <maulvi@users.noreply.github.com>
2026-08-13 18:31:00 +0700

    feat: auto-start tmux on container boot

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,8 @@ if [ -f /opt/data/ssh/authorized_keys ]; then
     chmod 600 ~/.ssh/authorized_keys
 fi
 
+# Start tmux session in background (auto-start)
+tmux new-session -d -s hermes
+
 # Start SSH daemon
 exec sudo /usr/sbin/sshd -D
